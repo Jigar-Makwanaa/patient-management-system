e6ded8f06e5697708feaf6a4f2870ceae8064d7e
Jigar Makawana <jigarmakwana34065@gmail.com>
2024-10-02 19:59:11 +0530

    modified file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './pages/Registration';
+
+function App() {
+  return (
+    <>
+      <Registration />
+    </>
+  );
+}
+
+export default App;
